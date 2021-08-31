35cb36f20d23b7308f2951ba1325cd053c52b598
Francisco <oficialkikolms123@gmail.com>
2021-08-31 12:59:25 +0100

    Some addictions rules to CreateSlugService

--- src/Services/CreateSlugService.js
+++ src/Services/CreateSlugService.js
@@ -1,6 +1,21 @@
 class CreateSlugService{
     async execute(url){
-        var slug = await createSlug(6);
+        //var slug = await createSlug(6);
+        var slug = "ayrt25"
+        var urlslug = "IU2P87"
+
+        var slugtest = "ayrt25";//temos de fazer uma query na bd com o slug que geramos, se houver uma igual fazemos o while
+        var urltest = "http://google.com"//tesmos de fazer uma query na bd
+
+        if(url == urltest){
+            return urlslug;
+        }
+
+        while(slug == slugtest){
+            slug = await createSlug(6);
+            console.log("slugtest: " + slugtest);
+            console.log("slug: " + slug);
+        }
 
         return slug;
     }
