82621bdfbd16ba73994914c4843b9092e1fb5282
Francisco <oficialkikolms123@gmail.com>
2021-09-13 16:01:26 +0100

    Implemented all of .env

--- src/Db.js
+++ src/Db.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(
-    '',
+    process.env.DB_CONN,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 var express = require('express');
+require('dotenv').config();
 
 var db = require('./db')
 var router = require('./routes');
-
-var PORT = 5555;
 
 var app = express();
 
 app.use(express.json());//enable express to read json stuff
 app.use(router);//use the routes
 
-app.listen(PORT, function(){
-    console.log(`Server running in port ${PORT}`);
+app.listen(process.env.PORT, function(){
+    console.log(`Server running in port ${process.env.PORT}`);
 });
